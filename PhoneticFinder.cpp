ce4837c03d27b422d5bdd4383ac8c808e4a381b2
ym758 <64576663+ym758@users.noreply.github.com>
2020-05-25 12:31:47 +0300

    Add files via upload

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,16 @@
+#include "PhoneticFinder.hpp"
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <exception>
+
+//using namespace std;
+namespace phonetic{
+std::string find(std::string text,std::string word){
+return text;
+    
+}
+}
+
+
+
